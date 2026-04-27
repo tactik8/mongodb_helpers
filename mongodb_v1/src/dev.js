75d594d20cd176e093c90008520d1dacc3a500c9
tactik8 <admin@tactik8.com>
2026-04-27 08:20:58 -0400

    go

---
+++ mongodb_v1/src/dev.js
@@ -0,0 +1,33 @@
+import { MongoDB } from './mongodb.js'
+
+let URI = 'mongodb://tactik8:Temp4now@192.168.2.243:27017/?authMechanism=DEFAULT'
+
+console.log('Start')
+
+
+async function test() {
+
+    let record = helpers.testRecord()
+
+
+    let db = new MongoDB()
+    db.uri = URI
+
+    db.databaseID = "n8n"
+    db.tenantID = "test"
+
+
+    // init db
+    let action = await db.init()
+
+
+    let act1 = await db.search()
+
+    console.log('r', act1.actionStatus)
+    
+
+
+
+}
+
+test()
--- mongodb_v1/src/index.js
+++ mongodb_v1/src/index.js
@@ -1,36 +1,8 @@
 
 import * as helpers from 'jsonld_helpers'
 
-import { MongoDB } from './mongodb.js'
+import * as mongodb from './mongodb.js'
+export * from './mongodb.js'
 
-let URI = 'mongodb://tactik8:Temp4now@192.168.2.243:27017/?authMechanism=DEFAULT'
+export default mongodb
 
-console.log('Start')
-
-
-async function test() {
-
-    let record = helpers.testRecord()
-
-
-    let db = new MongoDB()
-    db.uri = URI
-
-    db.databaseID = "n8n"
-    db.tenantID = "test"
-
-
-    // init db
-    let action = await db.init()
-
-
-    let act1 = await db.search()
-
-    console.log('r', act1.actionStatus)
-    
-
-
-
-}
-
-test()
